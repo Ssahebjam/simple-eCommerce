cd40fa4c6802ba5a510fd87287e4947b24a30643
soheyl <soheylrashidsahebjam@gmail.com>
2025-07-11 20:57:10 +0330

    add site header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
+import MainHeader from './componenets/MainHeader'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>hello</div>
+    <MainHeader />
   )
 }
 
--- src/componenets/MainHeader.jsx
+++ src/componenets/MainHeader.jsx
@@ -1,1 +1,7 @@
-function kalsdjflkadsjf
+const MainHeader = () => {
+    return (
+        <div> this is site header</div>
+    )
+}
+
+export default MainHeader;
